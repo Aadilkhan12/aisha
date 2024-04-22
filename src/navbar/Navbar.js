e1c5875273e2031ca7501f60b358bfcbe16b3f6c
Aadilkhan12 <rkaadil06@gmail.com>
2024-04-22 08:37:41 +0530

    navbar desing

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,74 @@
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import {
+  Bars3Icon,
+  ShoppingBagIcon,
+  XMarkIcon,
+  MagnifyingGlassIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
+
+export default function Navbar() {
+  return (
+    <Disclosure as="nav" className="bg-white-800 static">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w gap-5 px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className=" inset-y-0 left-0 flex items-center">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-black-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+                <button
+                  type="button"
+                  className="relative rounded-full bg-white-800 p-1 text-black-400 hover:text-gray- focus:outline-none  sm:hidden"
+                >
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">Search</span>
+                  <MagnifyingGlassIcon class="h-6 w-6" />
+                </button>
+              </div>
+              <div className="flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">Aasha</div>
+              </div>
+              <div className=" inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button
+                  type="button"
+                  className="relative rounded-full bg-white-800 p-1 text-black-400 hover:text-gray- focus:outline-none lg:hidden"
+                >
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">Search</span>
+                  <MagnifyingGlassIcon class="h-6 w-6" />
+                </button>
+                <button
+                  type="button"
+                  className="relative rounded-full bg-white-800 p-1 text-black-400   hover:text-gray- focus:outline-none "
+                >
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">User</span>
+
+                  <UserIcon class="h-6 w-6 " />
+                </button>
+                <button
+                  type="button"
+                  className="relative rounded-full bg-white-800 p-1 text-black-400 hover:text-gray-focus:outline-none "
+                >
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">Shoping</span>
+                  <ShoppingBagIcon class="h-6 w-6 " />
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </Disclosure>
+  );
+}
